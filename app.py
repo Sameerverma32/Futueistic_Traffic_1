77fa9beb38e9fbbc4746de0f9526bdb4efe50686
Sameerverma32 <sameerverma8318@gmail.com>
2025-09-03 11:18:58 +0530

    ml

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template, abort, redirect, url_for, 
+from flask import Flask, request, jsonify, render_template, redirect, url_for, session, flash
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+import joblib
+
+df = pd.read_csv('futuristic_city_traffic_sampled.csv')
+
+#load the saved model and preprocessing components
+model = joblib.load('traffic_density_model.pkl')
+scaler = joblib.load('scaler.pkl')
 
 app = Flask(__name__)
 
-
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
-    
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/base.html')
+def base():
+    return render_template('base.html')
