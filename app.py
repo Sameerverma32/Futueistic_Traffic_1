6be41b42f6ab998e2c8f38cf8ed1ae07a90bb2f3
Sameerverma32 <sameerverma8318@gmail.com>
2025-09-08 16:40:30 +0530

    ml

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ import joblib
 import os
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, request, jsonify, render_template, redirect, url_for, session, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_bcrypt import bcrypt
+
 
 app = Flask(__name__)
 app.config['SECRET KEY'] = 'your_secret_key_here'
@@ -20,6 +24,7 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
+    
     password = db.Column(db.String(150), nullable=False)
 
 df = pd.read_csv('futuristic_city_traffic_sampled.csv')
@@ -29,10 +34,42 @@ model = joblib.load('Analysis/traffic_model.pkl')
 scaler = joblib.load('Analysis/scaler.pkl')
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        existing_user = User.query.filter((User.username==username)|(User.email==email)).first()
+        if existing_user:
+            flash('Username or email already exists')
+        else:
+            hashed_password = generate_password_hash(password)
+            new_user = User(username=username, email=email, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Registration successful! Please login.')
+            return redirect(url_for('login'))
+    return render_template('login.html')
 
+# Login Route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email_or_username = request.form['email']
+        password = request.form['password']
+        
+        user = User.query.filter_by(email=email_or_username).first() or User.query.filter_by(username=email_or_username).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid email or password.', 'danger')
+    
     return render_template('login.html')
+
+
 
 @app.route('/')
 def index():
@@ -46,4 +83,25 @@ def about():
 def base():
     return render_template('base.html')
 
+def Vehicle_Type_Distribution():
+    fig = px.pie(df, names="Vehicle Type", title="Vehicle Type Distribution")  
+    graph1_html = pio.to_html(fig, full_html=False)
+    return graph1_html
+
+
+@app.route('/categorical_analysis')
+def categorical_analysis():
+    graph1 = Vehicle_Type_Distribution()
+    return render_template('job_analysis.html', graph1_html=graph1)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    if not os.path.exists('users.db'):
+        with app.app_context():
+            db.create_all()
 app.run(debug=True)
